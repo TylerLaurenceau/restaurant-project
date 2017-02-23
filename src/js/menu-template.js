37b834ab5567e26c9c5818174d337d42d0013829
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-23 15:34:08 -0500

    started on pricing

--- src/js/menu-template.js
+++ src/js/menu-template.js
@@ -5,11 +5,12 @@ function menuTemplate(data){
     console.log(data);
     for (var x = 0; x<data.appetizers.length; x++){
       $(".appetizers").append(`<div class = "foodTitle">${data.appetizers[x].item}:</div>
-                               <div class = "foodDescription">${data.appetizers[x].description}`)
+                               <span class = "foodPrice">${data.appetizers[x].price}</span>
+                               <div class = "foodDescription">${data.appetizers[x].description}</div>`)
     }
     for (var y = 0; y<data.entrees.length; y++){
-      $(".entrees").append(`<div class = "foodTitle">${data.appetizers[y].item}:</div>
-                            <div class = "foodDescription">${data.entrees[y].description}`)
+      $(".entrees").append(`<div class = "foodTitle">${data.entrees[y].item}:</div>
+                            <div class = "foodDescription">${data.entrees[y].description}</div>`)
     }
     for (var z = 0; z<data.sides.length; z++){
       $(".sides").append(`<div class = "foodTitle">${data.sides[z].item}</div>
--- src/js/news-template.js
+++ src/js/news-template.js
@@ -2,8 +2,8 @@ import $ from 'jquery';
 import {getNews} from './requests.js';
 
 function newsTemplate(data){
-  $(".latestNews").append(`<p class = "newsTitle">${data.title}</p>
-                   <p class = "newsDate">${data.date_published}</p>
+  $(".latestNews").append(`<div class = "newsTitle">${data.title}</div>
+                   <p class = "newsDate"><b>${data.date_published}</b></p>
                    <p class = "newsPost">${data.post}</p>`);
 }
 
