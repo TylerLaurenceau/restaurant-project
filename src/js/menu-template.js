b64b6f92aeba33609fc3f1e21a0bb77ea6468ec7
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-23 15:12:50 -0500

    got the menu evened up correctly. getting ready to add prices

--- src/js/menu-template.js
+++ src/js/menu-template.js
@@ -4,11 +4,11 @@ import {getmenu} from './requests.js';
 function menuTemplate(data){
     console.log(data);
     for (var x = 0; x<data.appetizers.length; x++){
-      $(".appetizers").append(`<div class = "foodTitle">${data.appetizers[x].item}</div>
+      $(".appetizers").append(`<div class = "foodTitle">${data.appetizers[x].item}:</div>
                                <div class = "foodDescription">${data.appetizers[x].description}`)
     }
     for (var y = 0; y<data.entrees.length; y++){
-      $(".entrees").append(`<div class = "foodTitle">${data.appetizers[y].item}</div>
+      $(".entrees").append(`<div class = "foodTitle">${data.appetizers[y].item}:</div>
                             <div class = "foodDescription">${data.entrees[y].description}`)
     }
     for (var z = 0; z<data.sides.length; z++){
