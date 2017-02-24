771f72c1b04e3c422441b0c7c696259d6c4df186
Marie Filbey <patriciamariefilbey@gmail.com>
2017-02-24 17:08:35 -0500

    Added allergy icons, still have to style them.

--- src/js/menu-template.js
+++ src/js/menu-template.js
@@ -6,27 +6,41 @@ function menuTemplate(data){
     for (var x = 0; x<data.appetizers.length; x++){
       $(".appetizers").append(`<div class = "foodTitle">${data.appetizers[x].item}:</div>
                                <div class = "foodPrice">$${data.appetizers[x].price}</div>
-                               <div class = "foodConent">
+                               <div class = "foodAllergies1">
+                                  <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                                  <i class="fa fa-heart" aria-hidden="true"></i>
+                                  <i class="fa fa-fire" aria-hidden="true"></i>
+                                  <i class="fa fa-viadeo" aria-hidden="true"></i>
+                               </div>
                                <div class = "foodDescription">${data.appetizers[x].description}</div>
-                               <div class = "foodAllergies1"></div>
-                               </div`)
-    //add a class to foodAllergies1 -- active class   if/if allergy = 1 add active class the current app.                        
+                               `)
+    //add a class to foodAllergies1 -- active class   if/if allergy = 1 add active class the current app.
     }
     for (var y = 0; y<data.entrees.length; y++){
       $(".entrees").append(`<div class = "foodTitle">${data.entrees[y].item}:</div>
                             <div class = "foodPrice">$${data.entrees[y].price}</div>
-                            <div class = "foodContent">
+                            <div class = "foodAllergies2">
+                                  <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                                  <i class="fa fa-heart" aria-hidden="true"></i>
+                                  <i class="fa fa-fire" aria-hidden="true"></i>
+                                  <i class="fa fa-viadeo" aria-hidden="true"></i>
+                            </div>
                             <div class = "foodDescription">${data.entrees[y].description}</div>
-                            <div class = "foodAllergies2"></div>
-                            </div>`)
+
+                            `)
     }
     for (var z = 0; z<data.sides.length; z++){
       $(".sides").append(`<div class = "foodTitle">${data.sides[z].item}</div>
                           <div class = "foodPrice">$${data.sides[z].price}</div>
-                          <div class = "foodContent">
+                          <div class = "foodAllergies3">
+                                  <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                                  <i class="fa fa-heart" aria-hidden="true"></i>
+                                  <i class="fa fa-fire" aria-hidden="true"></i>
+                                  <i class="fa fa-viadeo" aria-hidden="true"></i>
+                          </div>
                           <div class = "foodDescription">${data.sides[z].description}
-                          <div class = "foodAllergies3"></div>
-                          </div>`)
+
+                          `)
     }
 }
 
